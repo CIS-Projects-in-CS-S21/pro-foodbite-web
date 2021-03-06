cd51d6635d17731f766dcf067119871b180112ab
Andreas Gagas <tug25511@temple.edu>
2021-03-06 15:00:48 -0500

    Added folder structure

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom"; 
+
+
+
+export default function Routes() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
