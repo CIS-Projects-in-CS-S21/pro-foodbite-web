1c2d1a821d8600830daad9d05dcaf62d671a6e29
cis-xin-zhang <tug72884@temple.edu>
2021-03-12 17:29:58 -0500

    sign up layout

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,35 @@
-import React from "react"
+import React from "react";
+import {Group, LongButton,Input } from "../styles/FormElements"
+import styled from "styled-components"
+import {useHistory} from "react-router-dom"
+
+const Container = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items:center;
+`
 
 export default function SignUp() {
+  const history = useHistory();
+  const cancelClick = () =>{
+    history.push("/");
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <form>
+        <Group>
+          <label style={{textAlign:"left"}}>Email</label>
+          <Input type="text" />
+          <label style={{textAlign:"left"}}>Password</label>
+          <Input type="text" />
+        </Group>
+        <LongButton onClick={cancelClick}>Cancel</LongButton>
+
+        <LongButton>Submit</LongButton>
+      </form>
+    </Container>
   )
 }
