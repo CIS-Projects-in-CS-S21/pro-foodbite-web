7de71ca3a53c038fae0c9371c3c698001ff30cb5
Matthew Cohen <mcohen0365@gmail.com>
2021-03-13 13:00:15 -0500

    Removed sign out link for Settings and Management nav links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,11 +7,13 @@ import { Redirect } from 'react-router-dom';
 
 const Navigation = () => {
   const [redirect, setRedirect] = useState(false);
+  const [redirectUrl, setRedirectUrl] = useState("/");
   const { restaurant, sign_out } = useContext(UserContext);
 
   const handleSignOut = () => {
     sign_out()
       .then(() => {
+        setRedirectUrl("/");
         setRedirect(true);
       })
       .catch(err => {
@@ -23,7 +25,7 @@ const Navigation = () => {
   return (
     <Nav className="ml-auto">
       {redirect ? (
-        <Redirect to="/" />
+        <Redirect to={redirectUrl} />
       ) : (
         <Fragment />
       )}
@@ -33,8 +35,8 @@ const Navigation = () => {
           <Link className="nav-link" to="/orders">Orders</Link>
 
           <NavDropdown alignRight title="Account" id="basic-nav-dropdown">
-            <Link className="dropdown-item" onClick={handleSignOut} to="/">Settings</Link>
-            <Link className="dropdown-item" onClick={handleSignOut} to="/">Restaruant Management</Link>
+            <Link className="dropdown-item" to="/">Settings</Link>
+            <Link className="dropdown-item" to="/">Restaruant Management</Link>
 
             <NavDropdown.Divider />
             <Link className="dropdown-item" onClick={handleSignOut} to="/">Sign Out</Link>
